717b4b045842bced27ae1dd7adfeadca3d172474
yohhhs <yohhhs@163.com>
2019-03-04 17:31:32 +0800

    feat:add page

---
+++ src/components/table-edit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="table-edit">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'table-edit',
+    props: {},
+    data() {
+      return {}
+    },
+    components: {},
+    mixins: [],
+    created() {
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
